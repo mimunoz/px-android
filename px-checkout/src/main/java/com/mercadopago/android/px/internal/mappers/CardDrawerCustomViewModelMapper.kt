a1d9df224a95083f4463f9428dd236815eed725d
Cristian Gaggino <cristian.gaggino@mercadolibre.com>
2021-04-06 18:43:19 -0300

    * fix post remedy application selection (#2464)

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/mappers/CardDrawerCustomViewModelMapper.kt
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/mappers/CardDrawerCustomViewModelMapper.kt
@@ -3,9 +3,9 @@ package com.mercadopago.android.px.internal.mappers
 import com.meli.android.carddrawer.model.customview.SwitchModel
 import com.mercadopago.android.px.model.internal.CardDrawerSwitch
 
-object CardDrawerCustomViewModelMapper {
+internal object CardDrawerCustomViewModelMapper {
 
-    fun mapToSwitchModel(cardDrawerSwitch: CardDrawerSwitch?) = cardDrawerSwitch?.run {
+    fun mapToSwitchModel(cardDrawerSwitch: CardDrawerSwitch?, default: String) = cardDrawerSwitch?.run {
         val states = SwitchModel.SwitchStates(
             getStateForCardDrawer(states.checked),
             getStateForCardDrawer(states.unchecked)
@@ -23,6 +23,7 @@ object CardDrawerCustomViewModelMapper {
 
     private fun getStateForCardDrawer(state: CardDrawerSwitch.SwitchStates.State) = state
         .run { SwitchModel.SwitchStates.State(textColor, weight) }
+
     private fun getOptionsForCardDrawer(options: List<CardDrawerSwitch.Option>) = options
         .map { SwitchModel.SwitchOption(it.id, it.name) }
 }
