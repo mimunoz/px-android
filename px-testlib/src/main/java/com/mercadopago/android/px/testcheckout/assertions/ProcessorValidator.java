52514bbc1c8cb3245d9c8009ba98590335314089
Guche Mantuano <agustin.mantuano@mercadolibre.com>
2021-04-08 18:10:57 -0300

    Removes testlib modules

--- px-testlib/src/main/java/com/mercadopago/android/px/testcheckout/assertions/ProcessorValidator.java
+++
@@ -1,9 +0,0 @@
-package com.mercadopago.android.px.testcheckout.assertions;
-
-import androidx.annotation.NonNull;
-
-import com.mercadopago.android.px.core.SplitPaymentProcessor;
-
-public interface ProcessorValidator {
-    void validate(@NonNull final SplitPaymentProcessor.CheckoutData checkoutData);
-}
