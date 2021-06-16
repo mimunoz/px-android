467aeb570514d0934cd6d47a59ec7ace09960a0c
Franco Fusaro <franco.fusaro@mercadolibre.com>
2021-06-16 14:13:50 -0300

    Refactor of checkout use case

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/domain/CheckoutUseCase.kt
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/domain/CheckoutUseCase.kt
@@ -10,6 +10,7 @@ import com.mercadopago.android.px.model.exceptions.MercadoPagoError
 import com.mercadopago.android.px.model.internal.CheckoutResponse
 import com.mercadopago.android.px.model.internal.OneTapItem
 import com.mercadopago.android.px.tracking.internal.MPTracker
+import kotlinx.coroutines.delay
 
 internal class CheckoutUseCase (
     private val checkoutRepository: CheckoutRepositoryNew,
@@ -17,47 +18,38 @@ internal class CheckoutUseCase (
     override val contextProvider: CoroutineContextProvider = CoroutineContextProvider()
 ) : UseCase<CheckoutUseCase.CheckoutParams, CheckoutResponse>(tracker) {
     /* default */
-    private var retryHandler: Handler? = null
-    /* default */
     private var refreshRetriesAvailable = MAX_REFRESH_RETRIES
 
     override suspend fun doExecute(param: CheckoutParams): Response<CheckoutResponse, MercadoPagoError> {
-        val checkoutResponse = checkoutRepository.checkout()
-        if (param.prioritizedCardId.isNotNull()){
-            val retryAvailable: Boolean = --refreshRetriesAvailable > 0
-            val oneTap = checkoutResponse.oneTapItems
-            val (cardFound, retryNeeded) = findCard(oneTap, param.prioritizedCardId)
-            if (cardFound && (!retryNeeded || !retryAvailable)) {
-                handleCardFound(oneTap, param.prioritizedCardId, checkoutResponse)
-            } else if (retryAvailable) {
-                handleFindCardRetry(retryNeeded, param.prioritizedCardId)
-            } else {
-                return Response.Failure(MercadoPagoError("Exceeded max retries on new card", false))
+        var checkoutResponse = checkoutRepository.checkout()
+        param.prioritizedCardId?.let { prioritizedCardId ->
+            var findCardRes = findCard(checkoutResponse.oneTapItems, prioritizedCardId)
+            while (cardNotFoundOrRetryNeeded(findCardRes) && hasRefreshRetriesAvailable()) {
+                delay(findCardRes.retryDelay)
+                --refreshRetriesAvailable
+                checkoutResponse = checkoutRepository.checkout()
+                findCardRes = findCard(checkoutResponse.oneTapItems, prioritizedCardId)
             }
-            return Response.Success(checkoutResponse)
-        } else {
-            checkoutRepository.configure(checkoutResponse)
-            return Response.Success(checkoutResponse)
+            if (cardNotFoundOrRetryNeeded(findCardRes)) {
+                return Response.Failure(
+                    MercadoPagoError("Exceeded max retries on refresh new card", false))
+            }
+            refreshRetriesAvailable = MAX_REFRESH_RETRIES
+            checkoutRepository.sortByPrioritizedCardId(checkoutResponse.oneTapItems, prioritizedCardId)
         }
-    }
-
-    private fun handleFindCardRetry(retryNeeded: Boolean, cardId: String) {
-        val retryDelay = if (retryNeeded) LONG_RETRY_DELAY else DEFAULT_RETRY_DELAY
-        if (retryHandler == null) {
-            val thread = HandlerThread("MyInitRetryThread")
-            thread.start()
-            retryHandler = Handler(thread.looper)
-        }
-        retryHandler!!.postDelayed(Runnable { execute(CheckoutParams(cardId)) }, retryDelay.toLong())
-    }
-
-    private fun handleCardFound(oneTap: List<OneTapItem>, cardId: String, checkoutResponse: CheckoutResponse) {
-        refreshRetriesAvailable = MAX_REFRESH_RETRIES
-        checkoutRepository.sortByPrioritizedCardId(oneTap, cardId)
         checkoutRepository.configure(checkoutResponse)
+        return Response.Success(checkoutResponse)
     }
 
-    private fun findCard(oneTap: List<OneTapItem>, cardId: String): Pair<Boolean, Boolean> {
+    private fun hasRefreshRetriesAvailable(): Boolean {
+        return refreshRetriesAvailable > 0
+    }
+
+    private fun cardNotFoundOrRetryNeeded(findCardRes : FindCardResult): Boolean {
+        return (!findCardRes.cardFound || findCardRes.retryNeeded)
+    }
+
+    private fun findCard(oneTap: List<OneTapItem>, cardId: String): FindCardResult {
         var cardFound = false
         var retryNeeded = false
         for (node in oneTap) {
@@ -67,16 +59,22 @@ internal class CheckoutUseCase (
                 break
             }
         }
-        return Pair(cardFound, retryNeeded)
+        return FindCardResult(cardFound, retryNeeded)
     }
 
     data class CheckoutParams(
         val prioritizedCardId: String?
     )
 
+    private data class FindCardResult(
+        val cardFound: Boolean,
+        val retryNeeded: Boolean,
+        val retryDelay: Long = if (retryNeeded) LONG_RETRY_DELAY else DEFAULT_RETRY_DELAY
+    )
+
     companion object {
-        private const val MAX_REFRESH_RETRIES = 4
-        private const val DEFAULT_RETRY_DELAY = 500
-        private const val LONG_RETRY_DELAY = 5000
+        private const val MAX_REFRESH_RETRIES = 3
+        private const val DEFAULT_RETRY_DELAY = 500L
+        private const val LONG_RETRY_DELAY = 5000L
     }
 }
